0620b2a859b8bbc951699c6935be70330c238420
Lil_Tim <99663083+LilTim0@users.noreply.github.com>
2022-09-02 07:57:18 +0200

    2022.09.02

--- ALPHA_v.2022.09/interpreter.cpp
+++ ALPHA_v.2022.09/interpreter.cpp
@@ -10,6 +10,12 @@ typedef struct memoryData {
     std::string type;
     std::string name;
     std::string value;
+
+    void show_data() {
+        std::cout << "Type: " << type << std::endl;
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "Value: " << value << std::endl;
+    }
 } memoryData;
 
 // Vector with all datas
@@ -17,12 +23,17 @@ std::vector<memoryData> memory {
     { "str", "sys","Shard lang - ALPHA v.2022.09"}
 };
 
+// Symobls and operators
 std::string var_types[] = { "int", "double", "str", "char", "bool" };
 std::string assign_operator = "<<";
 char end_line = ';';
 
+// Functions prototypes
 std::vector<std::string> string_split(std::string string_to_split, std::string delimiter);
+bool interpret(std::string code);
+void to_code_lines(std::vector<std::string> code);
 
+// Code -> Interpreter function
 void to_code_lines(std::vector<std::string> code) {
     for(int i = 0; i < code.size(); i++) {
         try {
@@ -33,6 +44,7 @@ void to_code_lines(std::vector<std::string> code) {
     }
 }
 
+// Interpreter function
 bool interpret(std::string code) {
     // Terms split
     std::vector<std::string> terms = string_split(code, " ");
@@ -45,7 +57,24 @@ bool interpret(std::string code) {
 
             // Case declaration
             if(terms[1].back() == end_line) {
-                std::cout << "Case declaration";
+                std::cout << "Case declaration" << std::endl;
+                std::string type = terms[0];
+                std::string name = terms[1];
+                name.pop_back();
+                memoryData variable = { type, name , "" };
+                variable.show_data();
+            }
+            else if(terms[2] == assign_operator) {
+                std::cout << "Case intialization" << std::endl;
+                std::string type = terms[0];
+                std::string name = terms[1];
+                std::string value = "";
+                for(int j = 3; j < terms.size(); j++)
+                    value.append(terms[j] + " ");
+                value.pop_back();
+                value.pop_back();
+                memoryData variable = { type, name , value };
+                variable.show_data();
             }
         }
     }
@@ -53,7 +82,6 @@ bool interpret(std::string code) {
 }
 
 // Split function
-
 std::vector<std::string> string_split(std::string string_to_split, std::string delimiter) {
     std::vector<std::string> splitted_string;
     int index = 0;
